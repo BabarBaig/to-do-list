9265888ebe3494fa10d615b61b75cb124a680060
Babar Baig <BobBaig@outlook.com>
2022-01-26 00:24:34 -0500

    Icon file added

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function App() {
         }
     ])
     return (<>
-        {todos.map((todo, i) => <div key={i}>{todo.text}</div>)}
+        {todos.map((todo, i) =>
+            <div className="todo" key={i}>{todo.text}</div>)}
     </>)
 }
 
