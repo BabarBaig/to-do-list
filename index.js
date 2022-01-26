8a0c9d5d0dddd1ff5c412df6ff47711e2d7c4062
Babar Baig <BobBaig@outlook.com>
2022-01-25 23:31:01 -0500

    flesh-out the code

---
+++ index.js
@@ -0,0 +1,21 @@
+function App() {
+    const [todos, setToDos] = React.useState([
+        {   text: 'learn react',
+            isCompleted: false
+        },
+        {   text: 'Meet friend for lunch',
+            isCompleted: false
+        },
+        {   text: 'build todo app',
+            isCompleted: false
+        }
+    ])
+    return (<>
+        {todos.map((todo, i) => <div key={i}>{todo.text}</div>)}
+    </>)
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
